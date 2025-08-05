2c05f83d0995e44d59e0a147e60fc0b1ed7e9215
João Cláudio <tanakanunust@gmail.com>
2025-08-05 17:40:26 -0300

    Tipos e Variáveis - 4

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = 5;
+
+        numero= 10;
+        System.out.println(numero);
+
+        //final float pi = 3.14F;
+
+        //Não pode ser alterado por conta que coloquei constante final
+        //pi = 3.15f;
+    }
+}
